2c9ac4a19b4f502262154875a9a62675be42ce9d
Julien Palard <julien@palard.fr>
2021-09-26 22:57:27 +0200

    Package it.

--- habits/__main__.py
+++ habits/__main__.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 # The main entry point. Invoke as `python -m habit-tracker'.
 import os
+from pathlib import Path
 
 
 def main():
     from habits.application import run
+
     # Default sqlite database location
-    database_file = os.path.abspath(os.path.dirname(__file__) + '/../sqlite') + '/habit-tracker.db'
+    database_file = (
+        Path().home() / ".local" / "share" / "habit-tracker" / "habit-tracker.db"
+    )
     run(database_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
